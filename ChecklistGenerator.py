17d24aec603a126345a0fa6e7d9513031abe1fb9
benji4096 <49770024+benji4096@users.noreply.github.com>
2022-05-12 11:23:06 -0700

    cleanup

--- ChecklistGenerator.py
+++ ChecklistGenerator.py
@@ -4,13 +4,16 @@ class Item:
     def __init__(self, title, parameter = None, itemType = None):
         self.branches = []
         self.title = title
-        self.itemType = None
+        self.itemType = itemType
         self.parameter = parameter
 
     def print(self, depth = 0):
-        print("  "*depth, "»", self.title, sep = "")
+        a = ""
+        if (self.parameter is not None):
+            a = ": " + self.parameter
+        print("  "*depth, "»", self.title, a, sep = "")
         for branch in self.branches:
-            branch.print(depth + 1);
+            branch.print(depth + 1)
 
     def push(self, item):
         self.branches.append(item)
@@ -33,13 +36,7 @@ class Checklist:
         for conversion in convert:
             options = options.replace(conversion[0], conversion[1])
         
-        
-
         options = options.split('\n')
-
-        for option in options:
-            print(option)
-        print()
 
         for i, line in sorted(enumerate(options), reverse=True):
             containsAnOption = False
@@ -50,11 +47,12 @@ class Checklist:
                 options.pop(i)
 
         for option in options:
-            print(option)
+            option.split(':')
+        print(options)
         return options
 
     def printItems(self):
-        self.tree.print();
+        self.tree.print()
 
 
 options1 = "xTool D1 Laser Enclosure Fume Hood Kit - Default Title #MWS Options 4247556978\nSKU:\nHide Description\n_mws_required:\n_mws_qty:1\n_mws_cart:1223086\nEstimated between:Apr 28, 2022 - May 5, 2022\nPanel Thickness:Commercial Grade 1/4\" Panels (Strongly Recommended)\nPanel Type: Full Laser Filtering\nPrinted Bracket Kit: Please Include Brackets and Parts\nUSB Exhaust Fan:No USB Exhaust Fan - Includes 4\" Mounting Bracket\nUSB Lighting Kit:No thank you.\nDoor Pulls (2 pieces):Plastic\nFitted MDF Spoil Board:No Thank you\nExtended height lift kit:Yes Please Include the lift kit\nHardware Grade:Nickel Plated Steel\n"
@@ -64,5 +62,20 @@ options1 = "xTool D1 Laser Enclosure Fume Hood Kit - Default Title #MWS Options 
 xToolList = Checklist()
 
 xToolList.loadOptions("xTool D1", options1)
+
+bag1 = xToolList.tree.push(Item("bag 1"))
+bag1.push(Item("hardware", "1/4"))
+bag1.push(Item("Door Pulls", "Pl"))
+
+
+bag2 = xToolList.tree.push(Item("bag 2"))
+carbonFilter = bag1.push(Item("Carbon Filter"))
+carbonFilter.push(Item("fan", "12V"))
+
+
+bag3 = xToolList.tree.push(Item("bag 3"))
+
+
+
 
 xToolList.printItems()

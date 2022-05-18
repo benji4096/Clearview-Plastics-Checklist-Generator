e686833b80591149ce29032821da844b9c46649d
benji4096 <49770024+benji4096@users.noreply.github.com>
2022-05-18 11:21:07 -0700

    Create gui.py

---
+++ gui.py
@@ -0,0 +1,33 @@
+﻿
+import tkinter as tk
+import ChecklistGenerator as ch
+
+class WindowApplication(tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        self.pack()
+
+        self.textbox = tk.Entry()
+        self.textbox.pack()
+
+        self.textBoxContents = tk.StringVar()
+        self.textBoxContents.set("Default Text")
+        self.textbox["textvariable"] = self.textBoxContents
+        self.textbox["fg"] = "grey"
+
+        self.textbox.bind('<Key-Return>', self.generateChecklist)
+    def generateChecklist(self, event):
+        print("Textbox value", "\""+str()+"\"")
+        options1 = self.textbox.get()
+
+        checklist = ch.Checklist()
+        checklist.loadOptions("xTool D1", options1)
+        checklist.printItems()
+
+        checklist.saveFile()
+
+        
+
+root = tk.Tk()
+app = WindowApplication(root)
+app.mainloop()
